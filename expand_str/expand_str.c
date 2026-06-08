d3302861afa4617e3b199ae81511a12ec08ab5f4
Rodrigo Saraiva <rod.saraiva@icloud.com>
2026-06-08 14:37:07 +0100

    Add files via upload

---
+++ expand_str/expand_str.c
@@ -0,0 +1,32 @@
+#include <unistd.h>
+
+int main(int ac, char **av)
+{
+    int     i;
+    int     in_word;
+    char    *s;
+
+    if (ac != 2)
+        return (write (1, "\n", 1), 0);
+    i = 0;
+    in_word = 0;
+    s = av[1];
+    while (s[i])
+    {
+        if (s[i] != ' ' && s[i] != '\t')
+        {
+            if (in_word == 1)
+                write (1, "   ", 3);
+            while (s[i] && s[i] != ' ' && s[i] != '\t')
+            {
+                write (1, &s[i], 1);
+                i++;
+            }
+            in_word = 1;
+        }
+        else
+            i++;
+    }
+    write (1, "\n", 1);
+    return (0);
+}
