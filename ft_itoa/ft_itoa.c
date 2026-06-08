84c6543d141e76093d112da0eb2bc9a66d14d3d1
Rodrigo Saraiva <rod.saraiva@icloud.com>
2026-06-08 14:27:49 +0100

    Add files via upload

---
+++ ft_itoa/ft_itoa.c
@@ -0,0 +1,58 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+static int len(int n)
+{
+    int len;
+
+    len = 0;
+    if (n <= 0)
+    {
+        len++;
+        n = -n;
+    }
+    while (n != 0)
+    {
+        n /= 10;
+        len++;
+    }
+    return (len);
+}
+
+char	*ft_itoa(int nbr)
+{
+    long    n;
+    char    *s;
+    int     i;
+
+    n = nbr;
+    i = len(n);
+    s = malloc(sizeof(char) * (i + 1));
+    if (!s)
+        return (NULL);
+    s[i] = '\0';
+    if (n < 0)
+    {
+        n = -n;
+        s[0] = '-';
+    }
+    if (n == 0)
+        s[0] = '0';
+    while (n != 0)
+    {
+        i--;
+        s[i] = '0' + (n % 10);
+        n /= 10;
+    }
+    return (s);
+}
+
+int main(void)
+{
+    char *s;
+
+    s = ft_itoa(16);
+    printf("%s\n", s);
+    free(s);
+    return (0);
+}
